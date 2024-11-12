dd13fbb05ed527298858c390c5d7b2bcebd2ea85
zach-rongstad <zachrongstad@gmail.com>
2024-11-11 23:40:43 -0600

    create addition function

--- js/regCalcPageJS.js
+++ js/regCalcPageJS.js
@@ -2,14 +2,31 @@ let firstNumber = null;
 let secondNumber = null;
 let currentOperation = null;
 let isEnteringSecond = null;
+let timeToClear = false;
+const addition = "+";
+const subtraction = "-";
+const multiplication = "*";
+const division = "/";
 
 
 function clearTextArea() {
   document.getElementById("textBox").value = "";
+  firstNumber = null;
+  secondNumber = null;
+  currentOperation = null;
+  isEnteringSecond = false;
+  timeToClear = false;
 }
 
 function addToTextArea(num) {
-  document.getElementById("textBox").value += num;
+  if (timeToClear) {
+    document.getElementById("textBox").value = "";
+    document.getElementById("textBox").value += num;
+    timeToClear = false;
+  }
+  else {
+    document.getElementById("textBox").value += num;
+  }
 }
 
 function dontAddZero(num) {
@@ -68,8 +85,23 @@ function percentage()
   textInput.value = textValue / 100;
 }
 
-function addition(num1, num)
+function additionFunction()
 {
   let textInput = document.getElementById("textBox");
-  let textValue = parseFloat(textInput.value)
+  let ogNum = parseFloat(textInput.value);
+  if (currentOperation == null)
+  {
+    currentOperation = addition;
+    firstNumber = ogNum;
+    isEnteringSecond = true;
+    timeToClear = true;
+  }
+  else if (currentOperation == addition)
+  {
+    firstNumber += ogNum;
+    textInput.value = firstNumber;
+    isEnteringSecond = true;
+    timeToClear = true;
+  }
 }
+
