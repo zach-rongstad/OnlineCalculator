283856f6324e92b5c3d540ee520d8535a33e52b4
zach-rongstad <zachrongstad@gmail.com>
2024-12-01 13:15:04 -0600

    beginning of gpaCalcPage

--- js/gpaCalcPage.js
+++ js/gpaCalcPage.js
@@ -2,30 +2,89 @@ function $(id) {
   return document.getElementById(id);
 }
 
-function classReturn(className) {
-  return document.getElementsByClassName(className);
+
+let totalClasses = [];
+let totalCredits = 0;
+let totalGradePoints = 0;
+
+
+function calculateGPA() {
+  if (totalCredits === 0) {
+    $("gpa").textContent = "0.00";
+    return;
+  }
+  let gpa = totalGradePoints / totalCredits;
+  $("gpa").textContent = gpa.toFixed(2);
 }
 
 
-const A = 4.0;
-const APlus = 4.0
-const AMinus = 3.7;
-const BPlus = 3.3;
-const B = 3.0;
-const BMinus = 2.7;
-const CPlus = 2.3;
-const C = 2.0;
-const CMinus = 1.7;
-const DPlus = 1.3;
-const D = 1.0;
-const F = 0.0;
+let currClass = 1;
+$("addClass").addEventListener("click", function () {
+  let newClass = "class" + ++currClass;
+  let newGrade = "grade" + currClass;
+  let newCredits = "credits" + currClass;
 
-$("credits").addEventListener("change", function() {
-  let creditForClass = $("credits").value;
-  console.log(creditForClass);
-  if (creditForClass < 0)
-  {
-    window.alert("A class is at least worth .5 credits! Try again.");
-    $("credits").value = "";
-  }
+  let originalClass = document.querySelector('.class1');
+  let clonedClass = originalClass.cloneNode(true);
+
+
+  clonedClass.className = newClass;
+  let gradeDropdown = clonedClass.querySelector('select');
+  gradeDropdown.className = newGrade;
+  gradeDropdown.id = newGrade;
+
+  let creditsInput = clonedClass.querySelector('input[type="number"]');
+  creditsInput.className = newCredits;
+  creditsInput.id = newCredits;
+
+  gradeDropdown.selectedIndex = 0;
+  creditsInput.value = '';
+
+  document.querySelector('.semester1').appendChild(clonedClass);
+
+  addEventListenersToClass(gradeDropdown, creditsInput);
 });
+
+function addEventListenersToClass(gradeDropdown, creditsInput) {
+  gradeDropdown.addEventListener("change", function () {
+    updateClassGPA(gradeDropdown, creditsInput);
+  });
+
+  creditsInput.addEventListener("change", function () {
+    if (creditsInput.value < 0.5) {
+      window.alert("A class can't be worth less than 0.5 credits! Please re-enter the credits!");
+      creditsInput.value = '';
+      return;
+    }
+    updateClassGPA(gradeDropdown, creditsInput);
+  });
+}
+
+function updateClassGPA(gradeDropdown, creditsInput) {
+  let gradeValue = parseFloat(gradeDropdown.value) || 0;
+  let creditValue = parseFloat(creditsInput.value) || 0;
+
+  let classIndex = totalClasses.findIndex(c => c.id === gradeDropdown.id);
+  if (classIndex === -1) {
+    totalClasses.push({
+      id: gradeDropdown.id,
+      grade: gradeValue,
+      credits: creditValue
+    });
+    totalGradePoints += gradeValue * creditValue;
+    totalCredits += creditValue;
+  } else {
+    let existingClass = totalClasses[classIndex];
+    totalGradePoints -= existingClass.grade * existingClass.credits;
+    totalCredits -= existingClass.credits;
+
+    existingClass.grade = gradeValue;
+    existingClass.credits = creditValue;
+    totalGradePoints += gradeValue * creditValue;
+    totalCredits += creditValue;
+  }
+
+  calculateGPA();
+}
+
+addEventListenersToClass($("grade1"), $("credits1"));
