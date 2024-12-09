670e1497b347289c92b65ef05f8101d632d335a1
zach-rongstad <zachrongstad@gmail.com>
2024-12-09 00:26:52 -0600

    adding download feature to final grade page

--- js/finalGradeCalcPage.js
+++ js/finalGradeCalcPage.js
@@ -46,6 +46,27 @@ $("finalGradeButton").addEventListener("click", function() {
     let currentGradePercent = $("currentGrade").value;
     let needOnFinal = (desiredGradePercent - currentGradePercent * (1 - (finalGradePercent / 100))) / finalGradePercent;
     console.log(needOnFinal * 100);
-    window.alert("You'll need a " + needOnFinal * 100 + "% on your final to get a " + desiredGradePercent + "% in your class. Good luck!");
+    window.alert("You'll need a " + needOnFinal.toFixed(2) * 100 + "% on your final to get a " + desiredGradePercent + "% in your class. Good luck!");
+  }
+});
+
+$("pdfButton").addEventListener("click", () => {
+  if ($("desiredGrade").value == "" || $("finalGradePercentage").value == "" || $("currentGrade").value == "")
+  {
+      window.alert("Please enter all values to get your final grade.");
+  }
+  else
+  {
+    let desiredGradePercent = $("desiredGrade").value;
+    let finalGradePercent = $("finalGradePercentage").value;
+    let currentGradePercent = $("currentGrade").value;
+    let needOnFinal = (desiredGradePercent - currentGradePercent * (1 - (finalGradePercent / 100))) / finalGradePercent;
+    const blob = new Blob(["You'll need a " + needOnFinal.toFixed(2) * 100 + "% on your final to get a " + desiredGradePercent + "% in your class. Good luck!"], {type: "text/plain"});
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "finalGrade.txt";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 });
